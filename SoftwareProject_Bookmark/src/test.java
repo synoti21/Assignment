76cae9894519b5317ade713f24cbb4af5f1b743a
Jason Ahn <synoti21@naver.com>
2022-03-13 22:09:35 +0900

    03132

--- SoftwareProject_Bookmark/src/test.java
+++ SoftwareProject_Bookmark/src/test.java
@@ -70,10 +70,20 @@ class BookmarkList{
 //class that reads file and makes bookmark and bookmark list
 class FileReader{
     LineChecker lineChecker = new LineChecker();
+
     void lineParser(String str){
-        String[] tempBookmark = str.split(";,");
-        if(lineChecker.validityCheck(tempBookmark) == 1){
+        String[] temp = str.split(";|,");
+        if(lineChecker.validityCheck(temp) == 1){
+            /*for(int i = 0; i < temp.length; i++){
+                System.out.println("valid input : " + temp[i]); //TEST
+            }
+            System.out.println("\n");*/
+
             //make bookmark
+
+
+        }else{
+            System.out.println("invalid input");
         }
 
 
@@ -85,40 +95,59 @@ class FileReader{
 
 //class that checks validity of each line by using url pattern, and time pattern
 class LineChecker{
+    public String timeValidPattern = "^2\\d\\d\\d-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])_([0-1][0-9]|2[0-3]):[0-5][0-9]$"; //checks the formats of created time
+    public String urlValidPattern = "^http://\\S*$";
+
     class PatternChecker{
         public int urlCheck(String str){
-            if(str.matches()){
+            if(str.matches(urlValidPattern)){
+                //System.out.println(str + " valid");
                 return 1;
             }else{
+                //System.out.println(str + " invalid");
+
                 return 0;
             }
         }
         public int timeCheck(String str){
-            if(){
+            if(str.matches(timeValidPattern)){
+                //System.out.println(str + " valid");
+
                 return 1;
             }else{
+                //System.out.println(str + " invalid");
+
                 return 0;
             }
         }
     }
     int validityCheck(String[] str){ // checks each line if it satisfies Bookmark condition
         PatternChecker patternChecker = new PatternChecker();
-        if(patternChecker.urlCheck(str[0]) == 1){
-            if(patternChecker.timeCheck(str[1]) == 1){ //if all conditions are satisfied (url, time)
+        if(patternChecker.timeCheck(str[1]) == 1){
+            if(patternChecker.urlCheck(str[2]) == 1){ //if all conditions are satisfied (url, time)
+                return 1; // ***TEST***
+
                 //make Bookmark and bookmark list with groupname and memo
-            }else{ //if time pattern is invalid, then it's invalid
-                System.out.println("invalid input");
+            }else{ //if url pattern is invalid, then it's invalid
+                //System.out.println(str[1] + " invalid input , url \n");
+                return 0;
             }
-        }else{ //it could be name, so check the next token
-            if(patternChecker.urlCheck(str[0])==1){
-                if(patternChecker.timeCheck(str[1]) == 1){
+        }
+        else{ //it could be name, so check the next token
+            /*if(patternChecker.timeCheck(str[1])==1){
+                if(patternChecker.urlCheck(str[2]) == 1){
+                    return 1; // ***TEST***
+
                     //make Bookmark and Bookmark list with groupname and memo
                 }else{ //invalid
-                    System.out.println("invalid input");
+                   // System.out.println(str[2] + " invalid input, url\n");
+                    return 0;
                 }
             }else{ //invalid
-                System.out.println("invalid input");
-            }
+               // System.out.println(str[1] + " invalid input, time\n");
+                return 0;
+            }*/
+            return 0;
         }
         //checking process
     }
@@ -130,7 +159,6 @@ class LineChecker{
 class Test{
     public static void main(String[] args) {
         Bookmark bookmark = new Bookmark("http://asdf");
-        bookmark.print();
 
         File file = new File("/Users/jasonahn/IdeaProjects/SoftwareProject_BookMark/src/af.txt");
         FileReader filereader = new FileReader();
@@ -145,6 +173,8 @@ class Test{
         } catch (java.io.FileNotFoundException e){
             System.out.println("file not found");
         }
+
+
 
 
 

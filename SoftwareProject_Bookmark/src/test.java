0555b3c11996ce38ee84eaceb816c374e2ec8d81
Jason Ahn <synoti21@naver.com>
2022-04-11 23:36:12 +0900

    complete

--- SoftwareProject_Bookmark/src/test.java
+++ SoftwareProject_Bookmark/src/test.java
@@ -1,10 +1,11 @@
 import java.io.File;
-import java.sql.Array;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
-import java.util.regex.Pattern;
+import java.net.*;
 
 class Bookmark{
     //necessary
@@ -17,8 +18,7 @@ class Bookmark{
     public String memo= "";
 
     //validity check
-    public String timeValidPattern = "^2\\d\\d\\d-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])_([0-1][0-9]|2[0-3]):[0-5][0-9]$"; //checks the formats of created time
-    public String urlValidPattern = "^http://\\w*$";
+    public String urlValidPattern = "^https?://\\w*$";
 
 
     //constructor
@@ -39,67 +39,140 @@ class Bookmark{
         }else{
             System.out.println("Invalid Input");
         }
-
     }
 
     //method
     public void print(){
-        System.out.println(
-                (this.bookMarkName.isEmpty() ? "" : (this.bookMarkName + ", ")) +
-                this.createdTime+ ", " + this.url +
-                (this.groupName.isEmpty() ? "" : (", " + this.groupName)) +
-                (this.memo.isEmpty() ? "" : ", " + this.memo));
+        try{
+            System.out.println(
+                    ((this.bookMarkName + ",")) +
+                            this.createdTime+ "," + this.url +
+                            (("," + this.groupName)) +
+                            ("," + this.memo));
+        }catch(Exception e){
+            e.getMessage();
+        }
+
     }
-    public void modifyInfo(){}
-    public void addInfo(){}
-    public void deleteInfo(){}
-
-
-
 }
 class BookmarkList{
     Bookmark[] bookmarks;
+    File fileStream;
+    int index =0;
+
 
     //method
-    //public int numBookmarks(){};
-    //public Bookmark getBookmark(int i){};
-    public void mergeByGroup(){};
+    public int numBookmarks(){
+        return index;
+    };
+    public Bookmark getBookmark(int i){
+        if(i > index){
+            System.out.println("Range out of Bound. Returned void bookmark");
+            return new Bookmark();
+        }else{
+            return bookmarks[i];
+        }
+    };
+    public void mergeByGroup(){
+        Bookmark temp = new Bookmark();
+        try{
+            for(int i =0 ;i < index; i++){
+                for(int j =i+1; j<index; j++){
+                    if(bookmarks[i].groupName.equals(bookmarks[j].groupName)){
+                        temp = bookmarks[j];
+                        bookmarks[j] = bookmarks[i+1];
+                        bookmarks[i+1] = temp;
+                        i++;
+                    }
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
-   // public BookmarkList createBookmarkByFile(){};
+    };
 
-    public void bookmarkToFile(){};
+    FileReader filereader = new FileReader();
 
+    public void bookmarkToFile() {
+        File file = new File("/Users/jasonahn/IdeaProjects/SoftwareProject_BookMark/src/af2.txt");
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            for (int i = 0; i < index; i++) {
+                printWriter.print(this.bookmarks[index].bookMarkName + ";");
+                printWriter.print(this.bookmarks[index].createdTime + ";");
+                printWriter.print(this.bookmarks[index].url + ";");
+                printWriter.print(this.bookmarks[index].groupName + ";");
+                printWriter.print(this.bookmarks[index].memo + ";\n");
+            }
+            printWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
     //constructor
-    public BookmarkList(){}
+    public BookmarkList(String bookmarkFilename){
+        bookmarks = new Bookmark[100];
+        fileStream = new File(bookmarkFilename);
+        this.fillBookmarkList();
+    }
 
+
+    public void fillBookmarkList(){ //fill BookmarkList with Bookmarks
+        try{
+            Scanner scanner = new Scanner(fileStream);
+            while (scanner.hasNext()){
+                String str = scanner.nextLine();
+                if(str.startsWith("//") | str.isEmpty()){
+                    continue;
+                }else if(filereader.lineParser(str)){
+                    bookmarks[index] = filereader.instanceCreator();
+                    index++;
+                }else{
+                    System.out.println("in line : " + str);
+                };
+            }
+        } catch(IOException e){
+            System.out.println("Unknown BookmarkList data File");
+        }catch (Exception e){
+            e.getStackTrace();
+            System.out.println(e.getMessage());
+        }
+    }
 }
 
-//class that reads file and makes bookmark and bookmark list
+//class that reads file and makes bookmark instances
 class FileReader{
     LineChecker lineChecker = new LineChecker();
     String[] BookMarkInfo = new String[5]; //name, time, url, groupname, memo
-
-    void lineParser(String str){
-        String[] temp = str.split(";|,");
-        if(lineChecker.validityCheck(temp) == 1){
-            /*for(int i = 0; i < temp.length; i++){
-                System.out.println("valid input : " + temp[i]); //TEST
+    boolean lineParser(String str) throws Exception{
+        Arrays.fill(this.BookMarkInfo,"");
+        try{
+            String[] temp = str.split(";|,");
+            for(int i =0 ; i < temp.length; i++) {
+                temp[i] = temp[i].trim();
             }
-            System.out.println("\n");*/
-
-            //make bookmark info array
-            for(int i = 0 ; i < 5; i++){
-                if(!temp[i].isEmpty() | !temp[i].isBlank()){ //if content exists
+            if(lineChecker.validityCheck(temp) == 1){
+                for(int i = 0 ; i < temp.length; i++){
                     this.BookMarkInfo[i] = temp[i];
                 }
+                for(int i =0 ; i < temp.length; i++) {
+                    this.BookMarkInfo[i] = this.BookMarkInfo[i].trim();
+                }
+                return true;
+            }else{
+                return false;
             }
-
-        }else{
-            System.out.println("invalid input");
+        }catch(Exception e){
+            System.out.println("ERROR : " + e.getMessage());
         }
+        return false;
     }
 
-    Bookmark instanceCreator(){
+    Bookmark instanceCreator(){ //method that makes bookmark instances and fills bookmark list
         Bookmark temp = new Bookmark();
 
         temp.bookMarkName = this.BookMarkInfo[0];
@@ -111,100 +184,62 @@ class FileReader{
         return temp;
     }
 
-    BookmarkList listCreator(String str){
-        BookmarkList tempList = new BookmarkList(str);
-
-    }
-
-
 }
 
 //class that checks validity of each line by using url pattern, and time pattern
 class LineChecker{
     public String timeValidPattern = "^2\\d\\d\\d-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])_([0-1][0-9]|2[0-3]):[0-5][0-9]$"; //checks the formats of created time
-    public String urlValidPattern = "^http://\\S*$";
 
-    class PatternChecker{
-        public int urlCheck(String str){
-            if(str.matches(urlValidPattern)){
-                //System.out.println(str + " valid");
+    class PatternChecker{ //subclass of line checker that checks url, time pattern
+        public int urlCheck(String str) throws Exception{
+            try {
+                URL aurl = new URL(str);
                 return 1;
-            }else{
-                //System.out.println(str + " invalid");
-
-                return 0;
+            }catch(MalformedURLException e) {
+                System.out.print("MalformedURLException: wrong URL - Invalid URL Pattern OR No URL ; ");
             }
+            return 0;
         }
         public int timeCheck(String str){
-            if(str.matches(timeValidPattern)){
-                //System.out.println(str + " valid");
-
-                return 1;
-            }else{
-                //System.out.println(str + " invalid");
-
-                return 0;
+            try{
+                if(str.matches(timeValidPattern)){
+                    return 1;
+                }else if(str.isEmpty()) {
+                    System.out.print("Date Format Error -> No Created Time ");
+                    return 0;
+                }
+                else{
+                    System.out.print("Date Format Error -> Invalid Time Pattern ");
+                    return 0;
+                }
+            }catch(Exception e){
+                System.out.println("ERROR" + e.getMessage());
             }
+            return 0;
         }
     }
-    int validityCheck(String[] str){ // checks each line if it satisfies Bookmark condition
+    int validityCheck(String[] str) throws Exception{ // checks each line if it satisfies Bookmark condition
         PatternChecker patternChecker = new PatternChecker();
         if(patternChecker.timeCheck(str[1]) == 1){
             if(patternChecker.urlCheck(str[2]) == 1){ //if all conditions are satisfied (url, time)
                 return 1; // ***TEST***
-
-                //make Bookmark and bookmark list with groupname and memo
             }else{ //if url pattern is invalid, then it's invalid
-                //System.out.println(str[1] + " invalid input , url \n");
                 return 0;
             }
         }
-        else{ //it could be name, so check the next token
-            /*if(patternChecker.timeCheck(str[1])==1){
-                if(patternChecker.urlCheck(str[2]) == 1){
-                    return 1; // ***TEST***
-
-                    //make Bookmark and Bookmark list with groupname and memo
-                }else{ //invalid
-                   // System.out.println(str[2] + " invalid input, url\n");
-                    return 0;
-                }
-            }else{ //invalid
-               // System.out.println(str[1] + " invalid input, time\n");
-                return 0;
-            }*/
+        else{
             return 0;
         }
-        //checking process
     }
-
-
 }
 
 
 class Test{
     public static void main(String[] args) {
-        BookmarkList bookmarkList = new BookmarkList();
-        File file = new File("/Users/jasonahn/IdeaProjects/SoftwareProject_BookMark/src/af.txt");
-        FileReader filereader = new FileReader();
-
-        try{
-            Scanner scanner = new Scanner(file);
-
-            while (scanner.hasNext()){
-                String str = scanner.nextLine();
-                filereader.lineParser(str);
-            }
-        } catch (java.io.FileNotFoundException e){
-            System.out.println("file not found");
+        String str = "/Users/jasonahn/IdeaProjects/SoftwareProject_BookMark/src/af.txt";
+        BookmarkList bookmarkList = new BookmarkList(str);
+        for(int i = 0; i < bookmarkList.index; i++ ){
+            bookmarkList.bookmarks[i].print();
         }
-
-
-
-
-
-
-        //String pattern test
-
     }
 }
